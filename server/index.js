bc5913cc52fb304820c65baffb8e6f18d8d1136d
Evan Caldwell <evancald@sas.upenn.edu>
2018-11-03 09:59:35 -0600

    completed database setup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const controller = require('./controller');
+const massive = require('massive');
+
+const { DATABASE_STRING } = process.env;
+const port = 8080;
 
 const app = express();
 app.use(bodyParser.json());
 
-const port = 8080;
+massive(DATABASE_STRING)
+  .then(db => {
+    app.set('db', db);
+    console.log('database is connected');
+  })
+  .catch(err => {
+    console.log('database connection error', err);
+  })
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
